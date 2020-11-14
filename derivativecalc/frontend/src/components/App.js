2efcabb744655e4d3fe53771b2e6ceb5c898020d
bhaveshk658 <bhaveshk658@berkeley.edu>
2020-11-14 13:40:36 -0800

    Basic form

--- derivativecalc/frontend/src/components/App.js
+++ derivativecalc/frontend/src/components/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
+import SolutionPage from "./SolutionPage";
  
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return (<HomePage/>);
+        return (<div>
+                    <h1>Header</h1>
+                    <form>
+                        <fieldset>
+                            <label>
+                                <p>Enter your expression</p>
+                                <input expr="expression" />
+                            </label>
+                        </fieldset>
+                    </form>
+                </div>);
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App name="Bhavesh" />, appDiv);
+render(<App/>, appDiv);
